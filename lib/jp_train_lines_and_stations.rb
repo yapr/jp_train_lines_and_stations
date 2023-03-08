1c2875c073b05be02243736b5814a9b571db6cd3
yamitake <take.yapr@gmail.com>
2023-03-08 20:26:24 +0900

    first commit

---
+++ lib/jp_train_lines_and_stations.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jp_train_lines_and_stations/version"
+
+module JpTrainLinesAndStations
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/jp_train_lines_and_stations_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe JpTrainLinesAndStations do
+  it "has a version number" do
+    expect(JpTrainLinesAndStations::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
